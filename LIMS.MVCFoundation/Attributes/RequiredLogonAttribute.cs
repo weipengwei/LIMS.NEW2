b9fc3b2efbd9131756388671ca5ad35540650ece
weipengwei <1078901292@qq.com>
2017-06-14 09:36:23 +0800

    QQ

--- LIMS.MVCFoundation/Attributes/RequiredLogonAttribute.cs
+++ LIMS.MVCFoundation/Attributes/RequiredLogonAttribute.cs
@@ -51,33 +51,6 @@ namespace LIMS.MVCFoundation.Attributes
             return result;
         }
 
-
     }
-
-    public class AllowCorsAttribute : ActionFilterAttribute
-    {
-        private string[] _domains;
-
-        public AllowCorsAttribute(string domain)
-        {
-            _domains = new string[] {domain};
-        }
-
-        public AllowCorsAttribute(string[] domains)
-        {
-            _domains = domains;
-        }
-
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            var context = filterContext.RequestContext.HttpContext;
-            var host = context.Request.UrlReferrer?.Host;
-            if (host != null && _domains.Contains(host))
-            {
-                context.Response.AddHeader("Access-Control-Allow-Origin", $"http://{host}");
-                context.Response.AddHeader("Access-Control-Allow-Credentials", "true");
-            }
-            base.OnActionExecuting(filterContext);
-        }
-    }
+   
 }
